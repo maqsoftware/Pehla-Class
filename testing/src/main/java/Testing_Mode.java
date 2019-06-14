6c37b997c75ed53567709aade4c1a9c639632df3
Ayush Mantri <ayushm@maqsoftware.com>
2019-06-14 09:23:26 +0530

    some fix

--- testing/src/main/java/Testing_Mode.java
+++ testing/src/main/java/Testing_Mode.java
@@ -7,11 +7,11 @@ import java.util.Date;
 
 
 public class Testing_Mode {
-        public static final boolean testing = true; // enable testing mode
+        public static final boolean testing = false; // enable testing mode
         public static final boolean study_section = false;   //shows all modules in study section
-        public static final boolean night_mode = true;      // if in test mode this helps you to activate and deactivate night mode else as per the implementation
+        public static final boolean night_mode = false;      // if in test mode this helps you to activate and deactivate night mode else as per the implementation
         public static final boolean play_ground = false;  // if in test mode this helps you to activate and deactivate play ground else as per the implementation
-    public static long timemillis = 0;
+        public static long timemillis = 0;
     public static void main(String args[]){
         String myDate = "2019/04/12 16:00:45";
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
@@ -23,7 +23,6 @@ public class Testing_Mode {
         }
         timemillis = date.getTime();
         System.out.println(timemillis);
-
     }
     }
 
