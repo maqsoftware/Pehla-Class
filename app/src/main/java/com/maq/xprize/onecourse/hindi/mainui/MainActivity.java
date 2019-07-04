b854dca12c684ce722a19c0dc913605d21fdf257
RickyRiko305 <pranaym@maqsoftware.com>
2019-07-04 14:17:02 +0530

    removed the return type

--- app/src/main/java/com/maq/xprize/onecourse/hindi/mainui/MainActivity.java
+++ app/src/main/java/com/maq/xprize/onecourse/hindi/mainui/MainActivity.java
@@ -396,7 +396,7 @@ public class MainActivity extends Activity {
         if (backPressedTime + 2000 > System.currentTimeMillis()) {
 
             super.onBackPressed();
-            return;
+
         } else {
             Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
         }
